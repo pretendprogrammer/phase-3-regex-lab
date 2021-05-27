cc5401f8937f480b71be222b459fec348ce15fbb
Yosef <shmuli5770@gmail.com>
2021-05-27 12:20:56 -0400

    commit tree 96c6ca88bea3486f5e49a3d834a27446883d12ff

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,17 +1,19 @@
+require 'pry'
 def starts_with_a_vowel?(word)
+    !!word.match(/^[aeiou]/i)
 
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+    text.scan(/\bun\w*ing\b/)
 end
 
 def words_five_letters_long(text)
-
+    text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+    !!text.match(/^[A-Z].*\.$/)
 end
 
 def valid_phone_number?(phone)
